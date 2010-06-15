438fd1a98a9136c9eca948e8d90b76cdfa516610
louis <louis@0373f64b-0ee8-4d80-96b1-a83ff7afd3e3>
2010-06-15 08:02:20 +0000

    Only replace double spaces by &nbsp;

--- editablegrid_renderers.js
+++ editablegrid_renderers.js
@@ -24,7 +24,7 @@ CellRenderer.prototype._render = function(rowIndex, columnIndex, element, value)
 	while (element.hasChildNodes()) element.removeChild(element.firstChild);
 
 	// call the specialized render method
-	return this.render(element, typeof value == 'string' ? htmlspecialchars(value, 'ENT_NOQUOTES').replace(/\s/g, '&nbsp;') : value);
+	return this.render(element, typeof value == 'string' ? htmlspecialchars(value, 'ENT_NOQUOTES').replace(/\s\s/g, '&nbsp; ') : value);
 };
 
 CellRenderer.prototype.render = function(element, value) 
