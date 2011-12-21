2f377823a27f3804e77e4075aede2f1be6a943f1
Louis Antoine <louisantoinem@gmail.com>
2011-12-21 14:01:22 +0100

    tabs

--- examples/full/index.php
+++ examples/full/index.php
@@ -41,11 +41,11 @@
 		<link rel="stylesheet" href="../../extensions/autocomplete_variant_2/jquery.autocomplete.css" type="text/css" media="screen">
 		!-->
 
-	    <!-- include javascript files for the OpenFlashChart  library -->
-    	<script src="../../extensions/openflashchart/js/swfobject.js"></script>
-    	<script src="../../extensions/openflashchart/js/json/json2.js"></script>
-    	<script src="../../extensions/openflashchart/js-ofc-library/ofc.js"></script>
-    	<script src="../../extensions/openflashchart/js-ofc-library/open_flash_chart.min.js"></script>
+	    <!-- include javascript files for the OpenFlashChart library -->
+		<script src="../../extensions/openflashchart/js/swfobject.js"></script>
+		<script src="../../extensions/openflashchart/js/json/json2.js"></script>
+		<script src="../../extensions/openflashchart/js-ofc-library/ofc.js"></script>
+		<script src="../../extensions/openflashchart/js-ofc-library/open_flash_chart.min.js"></script>
 
 		<!-- include javascript and css files for this demo -->
 		<script src="javascript/demo.js" ></script>
