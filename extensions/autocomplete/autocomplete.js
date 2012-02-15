f432a625961adacb22620d92fbc0d9cbca8d04cc
Louis Antoine <louisantoinem@gmail.com>
2012-02-15 21:12:09 +0100

    Improved behavior of AutoCompleteEditor - 2 tests still fail on IE - not tested yet on Safari/Chrome

--- extensions/autocomplete/autocomplete.js
+++ extensions/autocomplete/autocomplete.js
@@ -15,6 +15,19 @@
  * @author Webismymind 
  */
 
+/* 
+ * Test suite:
+ * - quit with escape with match (IE/FF: OK)
+ * - quit with escape without match (IE/FF: OK)
+ * - save with click on suggestion (IE/FF: OK)
+ * - save with enter with match (IE/FF: OK)
+ * - save with enter without match (IE/FF: OK)
+ * - save with enter with match, but very quickly before suggestion appears (IE/FF: OK)
+ * - type to have a suggestion then continue to have no match and have the list disappear (IE/FF: OK)
+ * - save with blur (click elsewhere) with match (FF: OK/IE: KO)
+ * - save with blur (click elsewhere) without match (FF: OK/IE: KO)
+ */
+
 function AutocompleteCellEditor(config) 
 {
 	// default options
@@ -39,28 +52,31 @@ AutocompleteCellEditor.prototype.displayEditor = function(element, htmlInput)
 
 		open: function() {
 			// the field cannot be blurred until the autocomplete list has gone away
-			// otherwise the text field disappears before passing in 'select'
+			// otherwise the text field disappears before passing in 'select' when clicking on a suggestion
 			this.onblur_backup = this.onblur;
 			this.onblur = null;
 		},
 		
-		select: function(event, ui) { 
+		select: function(event, ui) {
 			// apply value when it has been selected either with a click or with ENTER
 			this.celleditor.applyEditing(this.element, ui.item.value); 
 		},
 
-		close: function() {
-			// call original onblur event
-			if (this.onblur_backup != null) this.onblur_backup();
+		close: function(event, ui) {
+			// call original onblur event, only if this close is due to a blurring of the suggestion list
+			// indeed, close can be called if there is no matching proposal anymore, it does necessarily not mean edition is done
+			// on FF eventPhase is 2 only when close is called due to a blurring
+			// on IE not, and so on IE we cannot blur the text field, unless I find something else to distinguish between the two types of close
+			if (event.eventPhase == 2 && this.onblur_backup != null) this.onblur_backup();
 		}
 	});	
 
-	// when pressing ENTER we need to close the autocomplete (otherwise it stays visible)
+	// when pressing ENTER we need to destroy the autocomplete
+	// otherwise it would stay visible if appearing after the text field was removed
 	var onkeydown = htmlInput.onkeydown;
 	htmlInput.onkeydown = function(event) {
 		event = event || window.event;
 		onkeydown.call(this, event);
-		if (event.keyCode == 13) $(htmlInput).autocomplete('close');
+		if (event.keyCode == 13) $(htmlInput).autocomplete('destroy');
 	};
-
 };
