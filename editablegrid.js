49a2533c680a7601f661c0807087498d264ca181
Louis Antoine-Moussiaux <louisantoine@louis-mac.local>
2012-05-06 12:00:02 +0200

    Fixed restore of page index when page count has become smaller.

--- editablegrid.js
+++ editablegrid.js
@@ -1572,11 +1572,11 @@ EditableGrid.prototype._rendergrid = function(containerid, className, tableid)
 EditableGrid.prototype.renderGrid = function(containerid, className, tableid)
 {
 	// restore stored parameters, or use default values if nothing stored
-	var pageIndex = this.localisset('pageIndex') ? parseInt(this.localget('pageIndex')) : 0;
+	var pageIndex = this.localisset('pageIndex') ? Math.min(parseInt(this.localget('pageIndex')), this.getPageCount() - 1) : 0;
 	this.sortedColumnName = this.localisset('sortColumnIndexOrName') && this.hasColumn(this.localget('sortColumnIndexOrName')) ? this.localget('sortColumnIndexOrName') : -1;
 	this.sortDescending = this.localisset('sortColumnIndexOrName') && this.localisset('sortDescending') ? this.localget('sortDescending') == 'true' : false;
 	this.currentFilter = this.localisset('filter') ? this.localget('filter') : null;
-
+	
 	// actually render grid
 	this.currentPageIndex = 0;
 	this._rendergrid(containerid, className, tableid);
