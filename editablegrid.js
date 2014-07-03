9a2971a3d0867b7bbd38fdb8ebc4a72ab92703be
Louis Antoine <louisantoine@louis-mac.local>
2014-07-03 10:29:42 +0200

    debug trace

--- editablegrid.js
+++ editablegrid.js
@@ -166,6 +166,7 @@ EditableGrid.prototype.init = function (name, config)
 	if (typeof this.detectDir != 'function') {
 		var error = new Error();
 		alert("Who is calling me now ? " + error.stack);
+		alert("And setCookie ? " + this.setCookie);
 	}
 
 	// private data
