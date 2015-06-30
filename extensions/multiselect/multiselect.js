8c3a5210257a0d2a5001b4869c46f97513e79c13
Louis Antoine <louisantoine@louis-mac.local>
2015-06-30 12:22:17 +0200

    Fixed interference between MultiselectCellEditor and Select2

--- extensions/multiselect/multiselect.js
+++ extensions/multiselect/multiselect.js
@@ -21,34 +21,34 @@ function MultiselectCellEditor(config)
 	this.maxWidth = 300;
 	this.titleIfEmpty = "Information";
 	this.messageIfEmpty = "There are no possible values to choose from!";
-	
+
 	// erase defaults with given options
 	this.init(config); 
 };
 
-// inherits SelectCellEditor functionalities
+//inherits SelectCellEditor functionalities
 MultiselectCellEditor.prototype = new SelectCellEditor();
 
-// redefine isValueSelected
+//redefine isValueSelected
 MultiselectCellEditor.prototype.isValueSelected = function(htmlInput, optionValue, value) { $(htmlInput).attr('multiple', true); return $.inArray(''+optionValue, this.valueArray) > -1; };
 
-// redefine getEditor
+//redefine getEditor
 MultiselectCellEditor.prototype.getEditor = function(element, value)
 {
 	// keep array of values
 	this.valueArray = value ? value.split(",") : [];
-	
+
 	// call base method
 	return SelectCellEditor.prototype.getEditor.call(this, element, value);
 };
 
-// redefine displayEditor to setup drop down checkboxes
+//redefine displayEditor to setup drop down checkboxes
 MultiselectCellEditor.prototype.displayEditor = function(element, htmlInput) 
 {
 	// check that we have values to choose from
 	var optionValues = this.column.getOptionValuesForEdit(element.rowIndex);
 	if (!optionValues || optionValues.length == 0) {
-		
+
 		$("<div class='jalert'>").html("<center>" + this.messageIfEmpty + "<center>").css('padding-top', '10px').dialog({
 			width: 350,
 			modal: !jQuery.browser.msie,
@@ -56,12 +56,12 @@ MultiselectCellEditor.prototype.displayEditor = function(element, htmlInput)
 			buttons: { "OK": function() { $(this).dialog("close"); } },
 			title: this.titleIfEmpty 
 		});
-		
+
 		return true;
 	}
-	
-	// call base method
-	SelectCellEditor.prototype.displayEditor.call(this, element, htmlInput);
+
+	// call base method (not the one from SelectCellEditor since it would use select2)
+	CellEditor.prototype.displayEditor.call(this, element, htmlInput);
 
 	// setup setup drop down checkboxes
 	$(htmlInput).dropdownchecklist({
@@ -70,24 +70,24 @@ MultiselectCellEditor.prototype.displayEditor = function(element, htmlInput)
 		width: Math.min(this.maxWidth, parseInt($(htmlInput).css('width'))) + 'px',
 		explicitClose: this.closeText,
 		onComplete: function(selector) {
-			
+
 			// when new values have been selected we apply them
 			var values = "";
-		    for (i=0; i < selector.options.length; i++) {
-		    	if (selector.options[i].selected && (selector.options[i].value != "")) {
-			    	if (values != "") values += ",";
-		    		values += selector.options[i].value;
-		    	}
-		    }
-		    
+			for (i=0; i < selector.options.length; i++) {
+				if (selector.options[i].selected && (selector.options[i].value != "")) {
+					if (values != "") values += ",";
+					values += selector.options[i].value;
+				}
+			}
+
 			htmlInput.celleditor.applyEditing(htmlInput.element, values);
 		}
 	});
-	
+
 	// cancel input events
 	htmlInput.onchange = null;
 	htmlInput.onblur = null;
-	
+
 	// correctly position created span
 	$(htmlInput).siblings('span').css({
 		'position': 'absolute', 
@@ -104,18 +104,24 @@ MultiselectCellEditor.prototype.displayEditor = function(element, htmlInput)
 	}
 };
 
+MultiselectCellEditor.prototype.cancelEditing = function(element) 
+{
+	// call base method (not the one from SelectCellEditor since it would use select2)
+	CellEditor.prototype.cancelEditing.call(this, element);
+};
+
 function MultiselectCellRenderer(config)
 { 
 	// default options
 	this.noneText = "(none)";
 	this.minWidth = 100;
 	this.maxWidth = 150;
-	
+
 	// erase defaults with given options
 	this.init(config); 
 }
 
-// inherits EnumCellRenderer functionalities
+//inherits EnumCellRenderer functionalities
 MultiselectCellRenderer.prototype = new EnumCellRenderer();
 
 MultiselectCellRenderer.prototype.render = function(element, value)
