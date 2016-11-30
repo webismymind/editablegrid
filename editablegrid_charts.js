cb00f81e71072d236b443c8c24bd06874aa7cc30
Louis Antoine <louisantoinem@gmail.com>
2016-11-30 18:23:17 +0100

    Quick and dirty fix to display max 100 categories on X axis... TO BE IMPROVED

--- editablegrid_charts.js
+++ editablegrid_charts.js
@@ -168,10 +168,14 @@ EditableGrid.prototype.renderBarChart = function(divId, title, labelColumnIndexO
 
 		// on category for each row
 		chart.xAxis.categories = []; 
+		var nb_categories = 0; for (var r = 0; r < rowCount; r++) if (!skipRow(r)) nb_categories++;
+		var mod = Math.floor(nb_categories / 100);
+		var row_index_display = 0;
 		for (var r = 0; r < rowCount; r++) {
 			if (skipRow(r)) continue;
+			row_index_display++;
 			var label = getRowAttribute(r, "barlabel"); // if there is a barlabel attribute, use it and ignore labelColumn
-			chart.xAxis.categories.push(label ? label : getValueAt(r, cLabel));
+			chart.xAxis.categories.push(nb_categories > 100 && row_index_display % mod != 0 ? "" : (label ? label : getValueAt(r, cLabel)));
 		}
 
 		// one serie for each bar column
